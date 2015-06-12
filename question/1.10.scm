0545c4cd6cd7f50413b2788e3fce74e7ffad137c
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-06-12 20:01:54 +0900

    wip

--- question/1.10.scm
+++ question/1.10.scm
@@ -46,3 +46,10 @@
 ;(define (h n) (A 2 n))
 ;(define (k n) (* 5 n n))
 
+
+(define (f n) (A 0 n)) ;f は2n 
+(define (g n) (A 1 n)) ;g は2^n
+(define (h n) (A 2 n)) ;h は
+(define (k n) (* 5 n n)) ;k は5n^2を計算
+
+
