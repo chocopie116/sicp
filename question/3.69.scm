9f3a7acb8c699a34924796becf72bd02fc125404
chocopie116 <chocopie116@gmail.com>
2015-12-16 20:47:20 +0900

    refactor 3.69

--- question/3.69.scm
+++ question/3.69.scm
@@ -23,11 +23,25 @@
                   (stream-cdr t))
       (pairs (stream-cdr s) (stream-cdr t)))))
 
+;(define (triples s t u)
+;  (cons-stream
+;    (list (stream-car s) (stream-car t) (stream-car u))
+;    (interleave
+;      (stream-map (lambda (x) (cons (stream-car s) x))
+;                  (pairs t (stream-cdr u)))
+;      (triples (stream-cdr s) (stream-cdr t) (stream-cdr u))
+;      )
+;    )
+;  )
+
 (define (triples s t u)
   (cons-stream
     (list (stream-car s) (stream-car t) (stream-car u))
     (interleave
-      (stream-map (lambda (x) (cons (stream-car s) x))
+      (stream-map (lambda (x) (list (stream-car s)
+                                    (car x)
+                                    (car (cdr x)))
+                    )
                   (pairs t (stream-cdr u)))
       (triples (stream-cdr s) (stream-cdr t) (stream-cdr u))
       )
