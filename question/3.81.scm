36b81ae7350258ab313c2e706908c9715c499760
chocopie116 <chocopie116@gmail.com>
2016-02-04 12:59:19 +0900

    コメント追記

--- question/3.81.scm
+++ question/3.81.scm
@@ -6,26 +6,29 @@
 (define (rand-update x)
   (+ x 1))
 
-(define (rand input-stream random-init)
+;request-stream 乱数に対する操作の命令のストリーム
+;random-init generate(rand-updateの引数)する元の値
+(define (rand request-stream random-init)
   (define random-stream
-    (if (stream-null? input-stream)
+    (if (stream-null? request-stream)
       the-empty-stream
-      (let ((request (stream-car input-stream)))
+      (let ((request (stream-car request-stream)))
         (cons-stream
           (cond ((eq? request 'generate) (rand-update random-init))
                 ((number? request) (rand-update request))
                 (else (error "Unknown request --- RAND" request)))
-          (rand (stream-cdr input-stream) (stream-car random-stream))))))
+          (rand (stream-cdr request-stream) (stream-car random-stream))))))
   random-stream)
 
-;数字はresetを示している
+;数字は与えられた数字でリセットせよという意味を示している
 (define request-stream
-  (cons-stream 100
+  (cons-stream 20
                (cons-stream 'generate
                             (cons-stream 'generate
-                                         (cons-stream 100
+                                         (cons-stream 1
                                                       (cons-stream 'generate
                                                                    (cons-stream 'generate
                                                                                 the-empty-stream)))))))
+
 
 (stream-head (rand request-stream 0) 6)
